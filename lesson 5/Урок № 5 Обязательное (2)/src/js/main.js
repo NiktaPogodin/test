7f9a48e571725fb23ff284b2fe142d864a681f01
Ник <pogodin_nikitka@bk.ru>
2019-04-27 18:28:39 +0300

    Урок № 5 Обязательное (2)

---
+++ lesson 5/Урок № 5 Обязательное (2)/src/js/main.js
@@ -0,0 +1,47 @@
+let startBtn = document.getElementById('start'),
+  budget = document.querySelectorAll('.budget-value'),
+  dayBudget = document.querySelectorAll('.daybudget-value'),
+  level = document.querySelectorAll('.level-value'),
+  expenses = document.querySelectorAll('.expenses-value'),
+  optionalExpenses = document.querySelectorAll('.optionalexpenses-value'),
+  income = document.querySelectorAll('.income-value'),
+  monthsavings = document.querySelectorAll('.monthsavings-value'),
+  yearsavings = document.querySelectorAll('.yearsavings-value'),
+
+  buttons = document.getElementsByTagName('button'),
+  count = document.querySelector('.count-budget-btn'),
+  accept1 = document.querySelector('.expenses-item-btn'),
+  accept2 = document.querySelector('.optionalexpenses-btn')
+  chooseIncome = document.querySelectorAll('.income'),
+  checkbox = document.querySelector('#savings'),
+  sum = document.querySelector('#sum'),
+  percent = document.querySelector('#percent'),
+  year = document.querySelectorAll('.year-value'),
+  month = document.querySelectorAll('.month-value'),
+  day = document.querySelectorAll('.day-value');
+
+console.log(startBtn);
+console.log(budget);
+console.log(dayBudget);
+console.log(level);
+console.log(expenses);
+console.log(optionalExpenses);
+console.log(income);
+console.log(monthsavings);
+console.log(yearsavings);
+console.log(buttons);
+console.log(count); 
+console.log(accept1);
+console.log(accept2);
+console.log(chooseIncome);
+console.log(checkbox);
+console.log(sum);
+console.log(percent);
+console.log(year);
+console.log(month);
+console.log(day);
+
+
+
+
+  
